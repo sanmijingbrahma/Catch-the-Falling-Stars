6bfd7a630197dc58f9d62d2d700313dc16caaee8
sanmijingbrahma <bsanmijing@gmail.com>
2024-10-29 16:58:22 +0530

    Made a circle{star} to fall top to bottom

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 using namespace std;
 
 
+class Ball{
+    public:
+        float radius;
+        int x, y;
+        float speed_y;
+    
+    void Draw(){
+        DrawCircle(x,y,radius,WHITE);
+    }
 
+    void Update(){
+        y+=speed_y;
+    }
+};
 
+Ball ball;
 
 int main()
 {
@@ -17,11 +31,23 @@ int main()
     InitWindow(scrren_width,scrren_height,"Catch the Falling Starts");
     SetTargetFPS(60); // Frame rate
 
+    ball.x=scrren_width/2-25;
+    ball.y=25;
+    ball.radius =25;
+    ball.speed_y= 7;
+
+    // Ball Properties
+
+
 
     // Game Loop
     while(!WindowShouldClose()){
         BeginDrawing();
+            ClearBackground(BLACK);
+            ball.Update();
 
+
+            ball.Draw();
             DrawRectangle(scrren_width/2 - 60,scrren_height-60,120,60,WHITE);
 
         EndDrawing();
